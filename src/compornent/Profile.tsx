154ff0fc03d3af92a752dab07d0e306c1a3480c1
yasuzaki25 <kaisei.yasuzaki@gmail.com>
2025-07-08 14:56:47 +0900

    ACFをprofile.tsxに適応完了

--- src/compornent/Profile.tsx
+++ src/compornent/Profile.tsx
@@ -21,16 +21,20 @@ type ProfileProps = {
 
 const TopAboutProfile = ({ profile, acf }: ProfileProps) => {
   const heading01 = acf.heading01 ? acf.heading01 : profile.heading01;
+  const imgUrl = acf.imgUrl ? acf.imgUrl : profile.imgUrl;
+  const heading02 = acf.heading02 ? acf.heading02 : profile.heading02;
+  const text = acf.text ? acf.text : profile.text;
+
   
   return (
     <div className={style.Profile}>
-      <h2>{profile.heading01}</h2>
+      <h2>{heading01}</h2>
       <div className={style.img}>
-        <Image src={profile.imgUrl} alt="hello" width={100} height={200} />
+        <Image src={imgUrl} alt="hello" width={100} height={200} />
       </div>
       <div className={style.Profile_Data}>
-        <h3>{profile.heading02}</h3>
-        <p>{profile.text}</p>
+        <h3>{heading02}</h3>
+        <p>{text}</p>
         <div className={style.Profile_Data_sns}>
           {profile.sns.map((item) => (
             <Link href={item.link}>
