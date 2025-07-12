b565b2a78c529b73e784646e1f8103e374b8e883
yassann20 <kaiseif4e@gmail.com>
2025-07-12 10:58:42 +0900

    auto-commit

--- src/compornent/Skills.tsx
+++ src/compornent/Skills.tsx
@@ -1,34 +1,36 @@
+// Skills.tsx
 "use client";
-
-import React from "react";
+import { useEffect, useState } from "react";
 import { Swiper } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 import style from "@/compornent/Skills.module.scss";
 import SkillsItem from "./Skills/SkillsItem";
 
-type SkillsProps = {
-  skills: {
-    heading: string;
-  };
-};
+const Skills = ({ skills }: { skills: { heading: string } }) => {
+  const [loaded, setLoaded] = useState(false);
 
-const Skills = ({ skills }: SkillsProps) => {
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
   return (
     <section className={style.Skill}>
       <h2>{skills.heading}</h2>
-      <Swiper
-        className={style.Skill_Slider}
-        modules={[Navigation, Pagination]}
-        spaceBetween={20}
-        slidesPerView={1}
-        navigation
-        pagination={{ clickable: true }}
-      >
-        <SkillsItem />
-      </Swiper>
+      {loaded && (
+        <Swiper
+          className={style.Skill_Slider}
+          modules={[Navigation, Pagination]}
+          spaceBetween={20}
+          slidesPerView={1}
+          navigation
+          pagination={{ clickable: true }}
+        >
+          <SkillsItem />
+        </Swiper>
+      )}
     </section>
   );
 };
